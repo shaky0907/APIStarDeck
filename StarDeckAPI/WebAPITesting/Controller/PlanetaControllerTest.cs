9e9a6e9d26e2660af208661ae96f59e7ff1f9fe7
David De la Hoz <divad0907@gmail.com>
2023-05-18 15:59:24 -0600

    Unit Testing
    Utilities unit testing Funcional
    cartas controller unit testing no funciona
    planetas controller unit testing no funciona

--- StarDeckAPI/WebAPITesting/Controller/PlanetaControllerTest.cs
+++ StarDeckAPI/WebAPITesting/Controller/PlanetaControllerTest.cs
@@ -1,20 +1,17 @@
 ﻿using FluentAssertions;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StarDeckAPI.Controllers;
 using StarDeckAPI.Data;
 using StarDeckAPI.Models;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
 
 namespace WebAPITesting.Controller
 {
-    public class CartaControllerTest
+    public class PlanetaControllerTest
     {
         private async Task<APIDbContext> GetDatabaseContext()
         {
@@ -26,21 +23,19 @@ namespace WebAPITesting.Controller
 
             databaseContext.Database.EnsureCreated();
 
-            if(await databaseContext.Carta.CountAsync() <= 0)
+            if (await databaseContext.Planeta.CountAsync() <= 0)
             {
-                for(int i = 0; i < 3; i++)
+                for (int i = 0; i < 3; i++)
                 {
-                    databaseContext.Carta.Add(
-                    new Carta()
+                    databaseContext.Planeta.Add(
+                    new Planeta()
                     {
-                        Id = "C-123141241"+i.ToString(),
-                        N_Personaje = "Carta"+i.ToString(),
-                        Energia =5,
-                        Imagen = "imagen"+i.ToString(),
-                        Raza = 1,
-                        Activa = true,
-                        Descripcion = "Carta Descripcion",
-                        Tipo = 1
+                        Id = "P-123141241" + i.ToString(),
+                        Nombre = "Planeta " + i.ToString(),
+                        Imagen = "Planeta " + i.ToString(),
+                        Tipo = 1,
+                        Estado = true,
+                        Descripcion = "Planeta Descripcion",
                     });
                     await databaseContext.SaveChangesAsync();
                 }
@@ -49,24 +44,21 @@ namespace WebAPITesting.Controller
         }
 
 
+
+
         [Fact]
-        public async void CartaController_GetAllCartas_ReturnsCartas()
+        public async void PlanetaController_GetPlanetas_ReturnsPlanetas()
         {
             //Arrange
-            var dbContext = await GetDatabaseContext();
-            var cartaController = new CartaController(dbContext);
+            var context = await GetDatabaseContext();
+            var controller = new PlanetaController(context);
 
             //Act
-            var result = cartaController.GetAll();
-            
+            var result = controller.GetPlanetas();
 
             //Assert
-
             Assert.NotNull(result);
-
-
         }
-
 
     }
 }
