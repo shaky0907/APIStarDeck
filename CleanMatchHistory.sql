bfd7059ed81e9734fe062911aa03774a3d22d807
Quigonar <67380073+Quigonar@users.noreply.github.com>
2023-06-03 17:57:17 -0600

    Create CleanMatchHistory.sql

---
+++ CleanMatchHistory.sql
@@ -0,0 +1,7 @@
+DELETE FROM CartasXTurnoXDeckXUsuario
+DELETE FROM CartasXTurnoXManoXUsuario
+DELETE FROM CartasXTurnoXPlanetaXUsuario
+DELETE FROM PlanetasXPartida
+DELETE FROM UsuarioXPartida
+DELETE FROM TurnoXUsuario
+DELETE FROM Partida
